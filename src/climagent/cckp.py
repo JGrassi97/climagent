61e151f4996e7bc936d4b5ec0b6ae7530d77e318
Jacopo Grassi <jacopo.grassi@polito.it>
2025-01-21 17:00:25 +0100

    scripts for local archive

--- src/climagent/cckp.py
+++ src/climagent/cckp.py
@@ -1,9 +1,10 @@
 import os, shutil
 import tempfile
-
+import requests
 import pandas as pd
 import xarray as xr
-
+import numpy as np
+from tqdm import tqdm
 import s3fs
 
 
@@ -12,38 +13,47 @@ def _get_url(collection : str, variable : str, dataset : str, scenario : str, pr
     """
     Build the url to request the file from the s3 bucket.
     """
-
-    base_url = 's3://wbg-cckp/data'
+    base_url = 'https://wbg-cckp.s3.amazonaws.com/data'
     url = f'{base_url}/{collection}/{variable}/{dataset}-{scenario}/{product}-{variable}-{aggregation}-{statistic}_{collection}_{dataset}-{scenario}_{type}_{percentile}_{period}.nc'
 
     return url
 
 
-def _request_dataset(collection : str, variable : str, dataset : str, scenario : str, product : str, aggregation : str, statistic : str, type : str, percentile : str, period : str) -> xr.Dataset:
-    """
-    Request the dataset from the s3 bucket.
-    """
+
+def _download_file(collection : str, variable : str, dataset : str, scenario : str, product : str, aggregation : str, statistic : str, type : str, percentile : str, period : str, dest_folder : str):
 
     url = _get_url(collection, variable, dataset, scenario, product, aggregation, statistic, type, percentile, period)
+    local_filename = os.path.join(dest_folder, os.path.basename(url))
 
     try:
-        fs = s3fs.S3FileSystem(anon=True)
-        f = fs.open(url)        
-        dataset = xr.open_dataset(f, engine='h5netcdf')
 
-        return dataset
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            total_size = int(r.headers.get('content-length', 0))
+            with open(local_filename, 'wb') as f, tqdm(
+                desc=local_filename,
+                total=total_size,
+                unit='iB',
+                unit_scale=True,
+                unit_divisor=1024,
+            ) as bar:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:  
+                        f.write(chunk)
+                        bar.update(len(chunk))
+        
+        return 0
     
     except:
-        print('File not found')
-        return None
+        return 1
 
 
 def _subset_dataset(dataset : xr.Dataset, lat : list, lon : list) -> xr.Dataset:
     """
     Subset the dataset to a specific lat and lon.
     """
-
-    dataset = dataset.sel(lat=slice(lat[0], lat[1]), lon=slice(lon[0], lon[1]))
+    if lat is not None and lon is not None:
+        dataset = dataset.sel(lat=slice(lat[0], lat[1]), lon=slice(lon[0], lon[1]))
 
     return dataset
 
@@ -58,25 +68,68 @@ def _clean_dataset(dataset : xr.Dataset, forbidden_list : list) -> xr.Dataset:
             dataset = dataset.drop_vars(forbidden_word)
 
     return dataset
+
+
+def _rename_variable(dataset : xr.Dataset, new_name : str) -> xr.Dataset:
+    """
+    Rename the variable in the dataset.
+    """
+
+    var_name = list(dataset.data_vars)[0]
+    dataset = dataset.rename_vars({var_name: new_name})
+
+    return dataset
     
 
 def _save_dataset(dataset: xr.Dataset, directory : str, name : str) -> None:
     """
     Save the dataset to csv
     """
+    var = list(dataset.data_vars)[0]
 
-    dataset_df = dataset.to_dataframe()
-    dataset_df.to_csv(f'{directory}/{name}.csv', )
+    dataset[var].encoding['_FillValue'] = np.nan
+    dataset[var].encoding['missing_value'] = np.nan
+
+    dataset.to_netcdf(f'{directory}/{name}.nc', )
+
+
+
+def _convert_timedelta_to_days(timedelta_str):
+    
+    return float(pd.Timedelta(timedelta_str).total_seconds() / 86400)
+
+
+def _convert_days(dataset : xr.Dataset) -> xr.Dataset:
+
+    var_name = list(dataset.data_vars)[0]
+    dataset[var_name] = xr.apply_ufunc(np.vectorize(_convert_timedelta_to_days), dataset[var_name])
+
+    return dataset
 
 
 
 def retrieve_dataset(collection, variable, dataset, scenario, product, aggregation, statistic, type, percentile, period,
-                     lat, lon, forbidden_list, directory, name):
+                     lat, lon, forbidden_list, cd, directory, name):
     """
     Retrieve the dataset from the s3 bucket, subset it and save it to csv.
     """
 
-    dataset = _request_dataset(collection, variable, dataset, scenario, product, aggregation, statistic, type, percentile, period)
-    dataset = _subset_dataset(dataset, lat, lon)
-    dataset = _clean_dataset(dataset, forbidden_list)
-    _save_dataset(dataset, directory, name)
+    stat = _download_file(collection, variable, dataset, scenario, product, aggregation, statistic, type, percentile, period, directory)
+
+    if stat == 1:
+        return 1
+
+    file_name = f'{directory}/{os.path.basename(_get_url(collection, variable, dataset, scenario, product, aggregation, statistic, type, percentile, period))}'
+    
+    with xr.open_dataset(file_name, engine='h5netcdf') as dataset:
+
+        dataset = _subset_dataset(dataset, lat, lon)
+        dataset = _clean_dataset(dataset, forbidden_list)
+        dataset = _rename_variable(dataset, variable)
+
+        if cd:
+            dataset = _convert_days(dataset)
+
+        _save_dataset(dataset, directory, name)
+
+    os.remove(file_name)
---
+++ src/scripts/climatology_archive.py
@@ -0,0 +1,97 @@
+import requests
+import os, shutil
+import xarray as xr
+from tqdm import tqdm
+import pandas as pd
+import argparse
+import warnings
+import multiprocessing
+
+from climagent.cckp import _download_file, retrieve_dataset
+
+
+def set_enviroment(path : str):
+
+    temp_dir = f'{path}/tmp'
+    store_dir = f'{path}/store'
+
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+
+    if not os.path.exists(store_dir):
+        os.makedirs(store_dir)
+
+    return temp_dir, store_dir
+
+
+# def download_temp_data(variables, statistics, periods, convert_days, temp_dir):
+#     """
+#     Download the files from the s3 bucket.
+#     """
+
+#     for var, stat, cd in zip(variables, statistics, convert_days):
+#         for period in periods:
+
+#             retrieve_dataset('era5-x0.25', var, 'era5-x0.25', 'historical', 'climatology', 'annual', stat, 'climatology', 'mean', period,
+#                         [40, 50], [0,20], ['lat_bnds', 'lon_bnds', 'bnds'], cd, temp_dir, f'{var}_{period}')
+
+
+
+# Funzione globale che sarà eseguita nei processi paralleli
+def task(args):
+    var, stat, cd, period, temp_dir = args
+    retrieve_dataset('era5-x0.25', var, 'era5-x0.25', 'historical', 'timeseries', 'annual', stat, 
+                     'timeseries', 'mean', period, None, None, 
+                     ['lat_bnds', 'lon_bnds', 'bnds'], cd, temp_dir, f'{var}_{period}')
+
+def download_temp_data(variables, statistics, periods, convert_days, temp_dir):
+    """
+    Download the files from the s3 bucket using multiprocessing.
+    """
+
+    tasks = [(var, stat, cd, period, temp_dir) for var, stat, cd in zip(variables, statistics, convert_days) for period in periods]
+
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        pool.map(task, tasks) 
+            
+
+def create_zarr_archive(variables, periods, temp_dir, store_dir):
+    """
+    Create a zarr archive from the downloaded files.
+    """
+    
+    dats = []
+    for var in variables:
+        for period in periods:
+
+            try:
+                dats.append(xr.open_dataset(f'{temp_dir}/{var}_{period}.nc'))
+            except:
+                pass
+
+
+    dats = xr.merge(dats)
+
+    dats.to_zarr(f'{store_dir}/era5-x0.25_timeseries.zarr', mode='w')
+
+
+
+def main():
+
+    warnings.filterwarnings("ignore")
+
+    path = '/Users/jacopograssi/Documents/climagent/example'
+
+    variables = ['tas', 'tasmin', 'tasmax', 'pr', 'cdd', 'cdd65', 'fd', 'hd30', 'hd35', 'hd40' , 'hd42', 'hd45', 'csdi','hdd65', 'hi', 'hi35', 'hi37', 'hi39', 'hi41', 'r20mm', 'r50mm', 'rx1day', 'rx5day','sd','td','tnn','tr','tr23','tr26','tr29','tr32','txx','wsdi']
+    statistics = ['mean', 'mean', 'mean', 'mean', 'max', 'mean', 'mean','mean','mean','mean','mean','mean','mean','mean','mean','mean','mean','mean','mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean', 'mean']
+    convert_days = [False, False, False, False, True, False, True, True, True, True, True, True, True, False, False, True, True, True, True, True, True, True, True, True, True, False, True, True, True, True,True, True, False, True]
+
+    periods = ['1950-2023']
+
+    temp_dir, store_dir = set_enviroment(path)
+    download_temp_data(variables, statistics, periods, convert_days, temp_dir)
+    create_zarr_archive(variables, periods, temp_dir, store_dir)
+
+    shutil.rmtree(temp_dir)
+    
+
