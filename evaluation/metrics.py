3cf97f6feaa33948775baaa78274d5b5123e8b23
Jacopo Grassi <jacopo.grassi@polito.it>
2025-03-15 21:53:27 +0100

    Create metrics.py

---
+++ evaluation/metrics.py
@@ -0,0 +1,36 @@
+from climagent.agent.climagent import ClimAgent
+import numpy as np
+
+from utils import deterministic_process_dataset
+
+
+
+def success_rate(dataset_ref, dataset_agent):
+
+    correct, failed = 0, 0
+    
+    if dataset_ref.dataset.equals(dataset_agent):
+        return 'correct'
+    else:
+        return 'failed'
+
+
+
+def repetition_rate(response):
+    # The Repetition Rate metric (RR) has been proposed in AgentQuest (Giocchini et al., 2024)
+
+    all_tools = []
+    results = {}
+    for id, message in enumerate(response['messages']):
+        
+        if message.name is not None:
+            all_tools.append(message.name)
+
+        all_tools_unique = np.unique(all_tools)
+        for tool in all_tools_unique:
+
+            results[tool] = all_tools.count(tool)
+
+    return results
+
+
