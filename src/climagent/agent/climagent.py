3ef6dd4656236b504d5b8e34358fd3afd9492c14
Jacopo Grassi <jacopo.grassi@polito.it>
2025-04-03 18:29:59 +0200

    Update climagent.py

--- src/climagent/agent/climagent.py
+++ src/climagent/agent/climagent.py
@@ -11,9 +11,10 @@ from langchain_core.messages import AnyMessage, ToolMessage, SystemMessage
 
 # Import tools and memory modules
 from climagent.tools.json_tools import JsonGetValueTool_custom, JsonListKeysTool_custom
-from climagent.tools.xarray_tools_indexing import SubsetDatasetTool
+from climagent.tools.xarray_tools_indexing import SubsetDatasetTool, SelectVariablesTool
 from climagent.tools.xarray_tools_grouping import ResampleTimeTool
 from climagent.tools.xarray_tools_aggregating import AggregateDatasetTool
+from climagent.tools.xarray_tools_look import LookDatasetTool
 from climagent.agent.prefix import PREFIX
 from climagent.agent.suffix import make_suffix
 
@@ -24,21 +25,24 @@ from climagent.state.agent_state import State
 
 
 class ClimAgent:
-    def __init__(self, dataset: xr.Dataset, llm, llm_temperature: float = 0.0):
+    def __init__(self, dataset: xr.Dataset, llm, state = State, llm_temperature: float = 0.0):
         
-    
+        
         # Load dataset
         self.dataset = dataset
         self.dataset_state = DatasetState(self.dataset)
         self.json_state = JsonState(self.dataset)
+        self.state = state
         
         # Initialize tools
         self.tools = [
             JsonGetValueTool_custom(json_state=self.json_state),
             JsonListKeysTool_custom(json_state=self.json_state),
             SubsetDatasetTool(dataset_state=self.dataset_state, json_state=self.json_state),
+            SelectVariablesTool(dataset_state=self.dataset_state, json_state=self.json_state),
             ResampleTimeTool(dataset_state=self.dataset_state, json_state=self.json_state),
-            AggregateDatasetTool(dataset_state=self.dataset_state, json_state=self.json_state)
+            AggregateDatasetTool(dataset_state=self.dataset_state, json_state=self.json_state),
+            LookDatasetTool(dataset_state=self.dataset_state, json_state=self.json_state)
         ]
         self.tools_names = {t.name: t for t in self.tools}
         
@@ -47,27 +51,29 @@ class ClimAgent:
         self.llm_planner = llm
         
         # Create agent
-        self.graph = self._build_agent()
+        self.graph = self._build_agent(self.state)
 
 
-    def _build_agent(self):
+    def _build_agent(self, state):
         
-        def planner(state: State):
+        def planner(state):
             messages = state['messages']
 
 
             plan_prompt = (
+                "You are a planner agent. Your goal is to create a structured plan for the analysis of a NetCDF dataset, basing on the request made by the user.\n"
+                "Do not make assumptions about the scope of the analysis, stay strictly to the query.\n"
                 "Please follow the steps below to create the analysis plan:\n"
                 "1. Identify the key objectives of the analysis.\n"
                 "2. Break the analysis into **clear sequential steps**.\n"
-                "3. Specify which tools (e.g., `SubsetDatasetTool`, `ResampleTimeTool`, `AggregateDatasetTool`) should be used in each step.\n"
+                "3. Specify which tools should be used in each step.\n"
                 "4. Define the **expected output** of each step.\n"
                 "5. Ensure the plan is **logical, structured, and efficient**.\n"
                 "\n"
                 "Example Format:\n"
-                "- **Step 1**: Select the geographic region → Use `SubsetDatasetTool` to filter dataset by latitude/longitude.\n"
-                "- **Step 2**: Resample time series data → Use `ResampleTimeTool` to aggregate data into larger time intervals.\n"
-                "- **Step 3**: Compute statistical metrics → Use `AggregateDatasetTool` to compute mean, min, max, etc.\n"
+                "- **Step 1**: Select the geographic region → Use `SubsetDatasetTool` to filter dataset by (...) with values (...).\n"
+                "- **Step 2**: Resample time series data → Use `ResampleTimeTool` to aggregate data in (...) dimension every (...).\n"
+                "- **Step 3**: Compute statistical metrics → Use `AggregateDatasetTool` to compute (...) on (...) cordinate.\n"
                 "- **Final Output**: A cleaned and structured dataset ready for visualization or further analysis.\n"
                 "\n"
                 "Now, based on the user's request, generate a custom plan.\n"
@@ -77,7 +83,7 @@ class ClimAgent:
             # Aggiungi il plan_prompt come SystemMessage
             plan_message = SystemMessage(content=plan_prompt)
 
-            messages = [plan_message]
+            messages = [plan_message] + state['messages']
             
             # Chiediamo all'LLM di generare il piano di analisi
             plan_message_response = self.llm_planner.invoke(messages)
@@ -88,12 +94,12 @@ class ClimAgent:
 
             return {'messages': messages + [plan_message_response] + [PREFIX] + [make_suffix(self.json_state)]}
         
-        def run_llm(state: State):
+        def run_llm(state):
             messages = state['messages']
             message = self.llm.invoke(messages)
             return {'messages': [message]}
 
-        def execute_tools(state: State):
+        def execute_tools(state):
             tool_calls = state['messages'][-1].tool_calls
             results = []
             for t in tool_calls:
@@ -111,11 +117,11 @@ class ClimAgent:
                 )
             return {'messages': results}
 
-        def tool_exists(state: State):
+        def tool_exists(state):
             result = state['messages'][-1]
             return len(result.tool_calls) > 0
         
-        graph_builder = StateGraph(State)
+        graph_builder = StateGraph(state)
         graph_builder.add_node("planner", planner)
         graph_builder.add_node("llm", run_llm)
         graph_builder.add_node("tools", execute_tools)
@@ -132,4 +138,4 @@ class ClimAgent:
     
     
     def run(self, messages: list[AnyMessage]):
-        return self.graph.invoke({'messages': messages},  config={"recursion_limit": 50}), self.dataset_state.dataset
+        return self.graph.invoke({'messages': messages},  config={"recursion_limit": 50})
